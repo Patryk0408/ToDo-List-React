af61b2d7e3e6bdad3b020bdb506333fd1b5c4f29
Patryk Krawczyk <patryk.krawczyk.it@gmail.com>
2023-06-26 21:23:45 +0200

    Add redux-saga

--- src/features/tasks/Buttons/index.jsx
+++ src/features/tasks/Buttons/index.jsx
@@ -1,12 +1,20 @@
 import { ButtonContainer, Button } from "./styled.js";
 import { useSelector, useDispatch } from "react-redux";
-import { selectTasks, toggleHideDone, setAllDone } from "../tasksSlice.js";
+import {
+  selectTasks,
+  toggleHideDone,
+  setAllDone,
+  fetchExampleTasks,
+} from "../tasksSlice.js";
 
 const Buttons = () => {
   const { tasks, hideDone } = useSelector(selectTasks);
   const dispatch = useDispatch();
   return (
     <ButtonContainer>
+      <Button onClick={() => dispatch(fetchExampleTasks())}>
+        Pobierz przykładowe zadania
+      </Button>
       {tasks.length > 0 && (
         <>
           <Button onClick={() => dispatch(toggleHideDone())}>
